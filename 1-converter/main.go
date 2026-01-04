0fe6e7e960b2957dc88ff14718415c0459d9a896
kirichenko <kirichenko.dmitriy1977@gmail.com>
2026-01-04 23:03:57 +1000

    Домашнее задание 2 выполнено

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+func inputAmount() {
+	var (
+		amount         int
+		sourceCurrency string
+		targetCurrency string
+	)
+	fmt.Scanln(&amount, &sourceCurrency, &targetCurrency)
+	fmt.Println(amount, sourceCurrency, targetCurrency)
+}
+
+func convertAmount(amount int, sourceCurrency, targetCurrency string) {
+
+}
+
 func main() {
 	const (
 		USDToEUR = 0.85
@@ -10,4 +24,5 @@ func main() {
 	eurToRub := (1.0 / USDToEUR) * USDToRUB
 	fmt.Println("Курс евро к рублю составляет")
 	fmt.Println(eurToRub)
+	inputAmount()
 }
