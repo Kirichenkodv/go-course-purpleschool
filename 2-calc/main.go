fd57132520cd918d74581c692dcea6482e860ef7
kirichenko <kirichenko.dmitriy1977@gmail.com>
2026-01-06 01:13:31 +1000

    Реализована функция selectOperation

--- 2-calc/main.go
+++ 2-calc/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import "fmt"
 
+const (
+	AVG = "avg"
+	SUM = "sum"
+	MED = "med"
+)
+
+func selectOperation() (operation string) {
+	for {
+		fmt.Println("Введите название операции из следующих возможных")
+		fmt.Println(AVG, SUM, MED)
+		if _, err := fmt.Scan(&operation); err != nil {
+			fmt.Println("Ошибка ввода. Повторите.")
+			continue
+		}
+		if operation == AVG || operation == SUM || operation == MED {
+			break
+		}
+		fmt.Println("Нет такой операции")
+
+	}
+	return
+}
+
+func main() {
+	operation := selectOperation()
+	fmt.Println("Вы выбрали:", operation)
 }
