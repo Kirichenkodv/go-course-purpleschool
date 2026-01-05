2f20787f0ce9142d52ca32c5c02e4aa3a809298e
kirichenko <kirichenko.dmitriy1977@gmail.com>
2026-01-05 19:15:52 +1000

    Реализована функция ввода данных с проверкой валидности

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -2,14 +2,81 @@ package main
 
 import "fmt"
 
-func inputAmount() {
-	var (
-		amount         int
-		sourceCurrency string
-		targetCurrency string
-	)
-	fmt.Scanln(&amount, &sourceCurrency, &targetCurrency)
-	fmt.Println(amount, sourceCurrency, targetCurrency)
+const (
+	usd = "usd"
+	eur = "eur"
+	rub = "rub"
+)
+
+func inputAmount() (sourceCurrency string, amount int, targetCurrency string) {
+	// 1) Ввод исходной валюты
+	for {
+		fmt.Println("Введите наименование начальной валюты из следующих возможных:")
+		fmt.Println(usd, eur, rub)
+		if _, err := fmt.Scan(&sourceCurrency); err != nil {
+			fmt.Println("Ошибка ввода. Повторите.")
+			continue
+		}
+		if sourceCurrency == usd || sourceCurrency == eur || sourceCurrency == rub {
+			break
+		}
+		fmt.Println("Неизвестная валюта. Повторите ввод.")
+	}
+	fmt.Println("Вы выбрали валюту", sourceCurrency)
+
+	// 2) Ввод суммы (целое число > 0)
+	for {
+		fmt.Println("Введите сумму (целое число > 0):")
+		if _, err := fmt.Scan(&amount); err != nil {
+			fmt.Println("Ошибка чтения суммы. Повторите.")
+			// сбросим amount на случай частичного ввода
+			amount = 0
+			continue
+		}
+		if amount > 0 {
+			break
+		}
+		fmt.Println("Сумма должна быть больше 0. Повторите.")
+	}
+
+	// 3) Ввод целевой валюты (исключая исходную)
+	for {
+		fmt.Println("Введите наименование конечной валюты из следующих возможных:")
+		switch sourceCurrency {
+		case usd:
+			fmt.Println(eur, rub)
+		case eur:
+			fmt.Println(usd, rub)
+		case rub:
+			fmt.Println(usd, eur)
+		}
+
+		if _, err := fmt.Scan(&targetCurrency); err != nil {
+			fmt.Println("Ошибка ввода. Повторите.")
+			continue
+		}
+
+		// проверка допустимости и исключение совпадения
+		valid := false
+		switch sourceCurrency {
+		case usd:
+			valid = (targetCurrency == eur || targetCurrency == rub)
+		case eur:
+			valid = (targetCurrency == usd || targetCurrency == rub)
+		case rub:
+			valid = (targetCurrency == usd || targetCurrency == eur)
+		}
+
+		if !valid {
+			fmt.Println("Недопустимая целевая валюта для выбранной исходной. Повторите.")
+			continue
+		}
+		break
+	}
+
+	fmt.Println("Вам надо переконвертировать", amount, sourceCurrency)
+	fmt.Println("Валюта для конвертации", targetCurrency)
+	return
 }
 
 func convertAmount(amount int, sourceCurrency, targetCurrency string) {
@@ -21,8 +88,6 @@ func main() {
 		USDToEUR = 0.85
 		USDToRUB = 80.44
 	)
-	eurToRub := (1.0 / USDToEUR) * USDToRUB
-	fmt.Println("Курс евро к рублю составляет")
-	fmt.Println(eurToRub)
+
 	inputAmount()
 }
