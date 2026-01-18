edb605c0a8f79c20cfafe288689a511a6567faa5
kirichenko <kirichenko.dmitriy1977@gmail.com>
2026-01-18 17:51:10 +1000

    Создана функция CreateBin

--- 3-struct/main.go
+++ 3-struct/main.go
@@ -1,6 +1,12 @@
 package main
 
-import "time"
+import (
+	"errors"
+	"fmt"
+	"strings"
+	"sync/atomic"
+	"time"
+)
 
 type Bin struct {
 	Id        string
@@ -10,6 +16,33 @@ type Bin struct {
 }
 
 var BinList = []Bin{}
+var lastNumber uint64
+
+func NextNumericId() string {
+	n := atomic.AddUint64(&lastNumber, 1)
+	return fmt.Sprintf("%d", n)
+}
+
+type CreateBinInput struct {
+	Name    string
+	Private bool
+}
+
+func CreateBin(input CreateBinInput) (Bin, error) {
+	name := strings.TrimSpace(input.Name)
+	if name == "" {
+		return Bin{}, errors.New("Поле Name не может быть пустым")
+	}
+	id := NextNumericId()
+	b := Bin{
+		Id:        id,
+		Private:   input.Private,
+		CreatedAt: time.Now(),
+		Name:      name,
+	}
+	BinList = append(BinList, b)
+	return b, nil
+}
 
 func main() {
 
